2fa5940edc83eaab05b7e8730f63ebf8442aa7cf
Antonia Siu <antonia.h.siu@gmail.com>
2015-08-29 12:55:41 +0100

    js to add colour classes to each letter

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,33 @@
 /* exported colourIt */
+/*global $ */
 
 function colourIt() {
+    var text = $("#originalText").val();
 
+    $('#colouredText').empty();
+    $('#colouredText').append(addColours(text));
+}
+
+function addColours(text) {
+    var colouredText = '',
+        c = '',
+        t = '';
+
+    for (var i = 0, len = text.length; i < len; i++) {
+        t = text[i];
+        c = t.charCodeAt(0);
+        if ((c > 47 && c < 58) || (c > 64 && c < 91) || (c > 96 && c < 123)) {
+            // alphanumeric
+            colouredText += '<span class="' + t + 'Col">' + t + "</span>";
+        } else if (c == 10) {
+            // linebreak
+            colouredText += '<br>';
+        } else if (!(c < 31) && !(c == 127)) {
+            // not a special char
+            colouredText += t;
+        }
+    }
+    return (colouredText);
 }
 
 // windowload
